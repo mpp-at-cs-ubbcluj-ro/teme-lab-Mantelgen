79f24f2ac39f240a58094b7605ea9f04576aa195
Mantelgen <84387756+Mantelgen@users.noreply.github.com>
2025-03-10 10:11:58 +0200

    Lab2-3
    Incercare de a pune proiectul de la labul 2 si labul 3 ( sa speram ca merge bine)

--- Lab2Gradle/src/main/java/ro/mpp2025/Main.java
+++ Lab2Gradle/src/main/java/ro/mpp2025/Main.java
@@ -1,3 +1,5 @@
+package ro.mpp2025;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -11,7 +13,5 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
-        System.out.println("Am printat ceva si adaug pe git");
-        System.out.println("Am printat ceva si adaug pe git");
     }
 }
